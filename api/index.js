1d1249a33dd5f8f3a564867893c726696fb1064a
Nuwan Wickramarachchi <nuwanwick99@gmail.com>
2023-12-20 10:18:20 +0200

    Create signup API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!!");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
