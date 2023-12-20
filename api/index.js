a0fb6960db8932b8f67b2198e426783781d255bc
Nuwan Wickramarachchi <nuwanwick99@gmail.com>
2023-12-20 09:30:41 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -18,3 +19,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!!");
 });
+
+app.use("/api/user", userRouter);
