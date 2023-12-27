18abf0569eb8729b7e575926333b4e10c0b3f19a
nuwan <nuwanwick99@gmail.com>
2023-12-27 09:35:42 +0200

    prepared files to deploy in render.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
     console.log(err);
   });
 
+const _dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -25,6 +28,12 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(_dirname, "/client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
+});
 
 //Middleware for error handling
 app.use((err, req, res, next) => {
