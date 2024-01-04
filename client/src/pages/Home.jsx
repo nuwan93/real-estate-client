b78016a0b3e8851af027a7a6032c6d26f39c5d8f
nuwan93 <nuwanwick99@gmail.com>
2024-01-04 13:30:16 +0200

    Coming soon update in the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="text-center">Home</div>;
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-3xl my-7">Coming Soon...</h1>
+      <p>
+        I am still working on this project. Sign In, Sign Up and Profile pages
+        are complete.
+      </p>
+      <p className="my-4">Please visit in few days to get more updates!!</p>
+    </div>
+  );
 }
